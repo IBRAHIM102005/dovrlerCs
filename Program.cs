d928560d1d9a154b6f5c04560fa58e88a6080736
IBRAHIM102005 <ibrahimmmdov5@gmail.com>
2023-10-12 22:04:54 +0400

    dovrler

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace dovrler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-dən n-ə qədər əddlərin faktorialını hesablayan proqram
+            int n=int.Parse(Console.ReadLine());
+            int s = 0;
+            if (n == 0 )
+                {
+                    s = 1;
+                
+                }
+            else if (n < 0)
+            {
+                Console.WriteLine("menfi ededin faktoriali olmur !!!");
+                
+            }
+            while (n > 0)
+            {
+                int h = 1;
+                for (int i = 1; i < n+1; i++)
+                {
+                    h = h * i;
+                   
+                }
+                s = s + h;
+                n= n - 1;
+            }
+            
+            Console.WriteLine(s);
+        }
+    }
+}
